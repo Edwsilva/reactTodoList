8ec2bffd0cd0b856fe37eec0d490bb547a8fbe34
Edwilson da Silva <edwilson.silva@rio.rj.gov.br>
2025-07-21 16:33:01 -0300

    Ajeitando o carregamento do .svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import Text from "./components/text";
+// Preciso pegar esse .svg e colocar como conteúdo em um retorno de um componente REACT
+// Pois o .svg é estático
+// Instalar o vite-plugin-svgr
+// npm i vite-plugin-svgr -D
+import trashIcon from "./assets/icons/trash.svg";
 
 export default function App() {
   return (
-    <div className="flex flex-col gap-2">
-      <Text
-        as="button"
-        onClick={() => alert("Cliquei!")}
-        // Extra (debug visual)
-        // Se quiser depurar visualmente qualquer elemento, use a classe do Tailwind outline ou border:
-        // className="border border-red-500"
-      >
-        Clique aqui
-      </Text>
-      <Text as="button" variant="button" onClick={() => alert("Cliquei!")}>
-        Clique aqui
-      </Text>
-      <Text variant="body-sm-bold" className="text-pink-base">
-        Olá mundo!
-      </Text>
-      <Text className="text-green-base">Olá mundo!</Text>
-      <Text variant="body-md-bold">Olá mundo!</Text>
-      <Text>Levar o dog pra passear</Text>
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-2">
+        <Text variant="body-sm-bold" className="text-pink-base">
+          Olá mundo!
+        </Text>
+        <Text className="text-green-base">Olá mundo!</Text>
+        <Text variant="body-md-bold">Olá mundo!</Text>
+        <Text>Levar o dog pra passear</Text>
+      </div>
+      <div className="flex gap-1">
+        <img src={trashIcon} />
+      </div>
     </div>
   );
 }
