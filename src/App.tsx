1c47bae7d5ff839b2f86e560c7550e81bc4fdd00
Edwilson da Silva <edwilson.silva@rio.rj.gov.br>
2025-07-21 16:17:01 -0300

    Ajuste do componente text para ser mais genérico

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,18 @@ import Text from "./components/text";
 export default function App() {
   return (
     <div className="flex flex-col gap-2">
+      <Text
+        as="button"
+        onClick={() => alert("Cliquei!")}
+        // Extra (debug visual)
+        // Se quiser depurar visualmente qualquer elemento, use a classe do Tailwind outline ou border:
+        // className="border border-red-500"
+      >
+        Clique aqui
+      </Text>
+      <Text as="button" variant="button" onClick={() => alert("Cliquei!")}>
+        Clique aqui
+      </Text>
       <Text variant="body-sm-bold" className="text-pink-base">
         Olá mundo!
       </Text>
