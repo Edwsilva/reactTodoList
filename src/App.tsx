7549d6bf0684669cb658ef0e4dbd5e5de9d40e7b
Edwilson <edwsilva@gmail.com>
2025-07-22 17:54:42 -0300

    Criando botão genérico

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import PlusIcon from "./assets/icons/plus.svg?react";
 import SpinnerIcon from "./assets/icons/spinner.svg?react";
 import Icon from "./components/icon";
 import Badge from "./components/badge";
+import Button from "./components/button";
 
 export default function App() {
   return (
@@ -39,6 +40,16 @@ export default function App() {
         <Badge variant="primary" className="secondary">
           2 de 5
         </Badge>
+
+        <div>
+          <Button icon={PlusIcon}>Nova tarefa</Button>
+          <Button icon={PlusIcon} variant="secondary">
+            Nova tarefa
+          </Button>
+          <Button icon={PlusIcon} variant="secondary" size="lg">
+            Nova tarefa
+          </Button>
+        </div>
       </div>
     </div>
   );
