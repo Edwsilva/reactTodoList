90449bdf7149b539db75577c2b36d4ae3f9401e3
Edwilson <edwsilva@gmail.com>
2025-07-23 11:08:38 -0300

    Criação do componente input

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Icon from "./components/icon";
 import Badge from "./components/badge";
 import Button from "./components/button";
 import ButtonIcon from "./components/button-icon";
+import InputText from "./components/input-text";
 
 export default function App() {
   return (
@@ -55,6 +56,10 @@ export default function App() {
           <ButtonIcon icon={TrashIcon} disabled />
           <ButtonIcon icon={TrashIcon} variant="secondary" />
           <ButtonIcon icon={TrashIcon} variant="tertiary" />
+        </div>
+
+        <div>
+          <InputText />
         </div>
       </div>
     </div>
